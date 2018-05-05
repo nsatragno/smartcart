e45a1af6c6f3b0908b2765db577bf0b4c46df4ff
Nina Satragno <nsatragno@gmail.com>
2018-05-05 16:38:20 -0300

    Agregar usuario por default.

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Usuario.create(email: "nsatragno@gmail.com", password: "12345678")
