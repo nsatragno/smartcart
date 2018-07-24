690148a53b555426e3c1abdaf9cb42df1e633919
Marcos Munoz <munozmarcos@users.noreply.github.com>
2018-07-23 23:31:35 -0300

    Android Project

---
+++ App/app/src/main/java/ar/com/smartcart/smartcart/MainActivity.java
@@ -0,0 +1,13 @@
+package ar.com.smartcart.smartcart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ly_activity_main);
+    }
+}
